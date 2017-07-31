d1711b0eb4c9096423d653a27c4b79cb01f5dbe9
Luciano Ramalho <luciano@ramalho.org>
2017-07-31 03:13:18 +0000

    signs_server.py

--- unicode/servers/signs_server.py
+++ unicode/servers/signs_server.py
@@ -9,8 +9,13 @@ import unicodedata
 PORT = 8000
 
 async def usage(request):
-    return web.Response(text='use /<word> to get list of characters '
-                             'with <word> in their Unicode names')
+    text = (
+        'use\t/index/«word» to get list of characters '
+        'with «word» in their Unicode names\n'
+        '\t/name/«c» to get Unicode name of character «c»'
+
+    )
+    return web.Response(text=text)
 
 
 async def index_for(request):
@@ -23,10 +28,11 @@ async def index_for(request):
 
 async def char_name(request):
     char = request.match_info.get('char', '')
-
+    if len(char) > 1:
+        raise web.HTTPBadRequest(text='Only one character per request is allowed.')
     name = unicodedata.name(char, None)
     if name is None:
-        raise(HTTPNotFound('No name for character {} in Unicode 9.'.format(char)))
+        raise web.HTTPNotFound(text='No name for character {} in Unicode 9.'.format(char))
     text = f'U+{ord(char):04x}\t{char}\t{name}'
     return web.Response(text=text)
 
@@ -38,7 +44,7 @@ if __name__ == '__main__':
     app = web.Application()
     app.router.add_get('/', usage)
     app.router.add_get('/index/{word}', index_for)
-    app.router.add_get('/char/{char}', char_name)
+    app.router.add_get('/name/{char}', char_name)
 
     print('Listening on port', PORT)
     web.run_app(app, port=PORT)
